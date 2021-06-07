5f4cbb0765ab5ea57680550f7bb87b259e9a2c86
charlie42 <oninfrared@gmail.com>
2021-06-07 16:38:40 +0200

    legend and gauges

--- src/pages/dashboard-demo3.tsx
+++ src/pages/dashboard-demo3.tsx
@@ -30,9 +30,13 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
+  Image,
+  SimpleGrid
 } from "@chakra-ui/react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ErrorBar } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ErrorBar, ReferenceLine } from 'recharts';
 import GaugeChart from 'react-gauge-chart'
+import ReactSpeedometer from "react-d3-speedometer"
+
 
 const domains = `
 111Specific phobias 
@@ -95,8 +99,8 @@ const DashboardDemo: FunctionComponent = () => {
   }, []);
 
   const getArcsLength = (errors, value) => {
-    console.log(errors, value, [value - errors[0]/10, errors[0]/10 + errors[1]/10, 1 - (value+errors[1]/10)])
-    return [value - errors[0]/10, errors[0]/10 + errors[1]/10, 1 - (value+errors[1]/10)]
+    console.log(errors[0], " ", errors[1], value, [value - errors[0]/10, errors[0]/10 + errors[1]/10 + 0.001, 1 - (value+errors[1]/10)])
+    return [value - errors[0]/10, errors[0]/10 + errors[1]/10 + 0.001, 1 - (value+errors[1]/10)]
   }
 
   const getData = () => {
@@ -115,8 +119,8 @@ const DashboardDemo: FunctionComponent = () => {
             "domain": "habits and repetitive behaviours",
             "scaled-follow-up-score": 2,
             "color-intensity-follow-up": 0.3,
-            "errors-entry-level": [2, 5],
-            "errors-follow-up": [1, 5]
+            "errors-entry-level": [2, 3],
+            "errors-follow-up": [1, 4]
         }, 
         {
             "entry-level-score": 4, 
@@ -126,7 +130,7 @@ const DashboardDemo: FunctionComponent = () => {
         {
             "entry-level-score": 4, 
             "domain": "risk taking",
-            "errors-entry-level": [2, 4],
+            "errors-entry-level": [2, 2],
         }, 
         {
             "entry-level-score": -2, 
@@ -134,7 +138,7 @@ const DashboardDemo: FunctionComponent = () => {
             "scaled-follow-up-score": -4,
             "color-intensity-follow-up": 1,
             "errors-entry-level": [1, 1],
-            "errors-follow-up": [0, 5]
+            "errors-follow-up": [0, 3]
         }
       ];
     } else {
@@ -152,7 +156,7 @@ const DashboardDemo: FunctionComponent = () => {
             "domain": "habits and repetitive behaviours",
             "scaled-follow-up-score": 3,
             "color-intensity-follow-up": 0.4,
-            "errors-entry-level": [3, 5],
+            "errors-entry-level": [3, 1],
             "errors-follow-up": [1, 2]
         }, 
         {
@@ -171,7 +175,7 @@ const DashboardDemo: FunctionComponent = () => {
             "scaled-follow-up-score": -4,
             "color-intensity-follow-up": 1,
             "errors-entry-level": [1, 1],
-            "errors-follow-up": [0, 5]
+            "errors-follow-up": [0, 3]
         }
       ];
     }
@@ -196,7 +200,9 @@ const DashboardDemo: FunctionComponent = () => {
             <Heading as="h3" size="sm">
               Behavior
             </Heading>
+            <Image src="media/legend-long.png" alt="Legend"/>
             <Center overflow="hidden" maxW="100%" fontSize="xs">
+              
               <BarChart
                 width={800}
                 height={600}
@@ -209,7 +215,8 @@ const DashboardDemo: FunctionComponent = () => {
                   bottom: 5,
                 }}
               >
-                <CartesianGrid strokeDasharray="3 3" />
+                <ReferenceLine x={0} stroke="blue" label="" />
+                
                 <YAxis 
                   dataKey="domain" 
                   type="category"
@@ -239,39 +246,95 @@ const DashboardDemo: FunctionComponent = () => {
                   <ErrorBar dataKey="errors-follow-up" width={4} strokeWidth={3} stroke="green" opacity={0.8} direction="x" />
                 </Bar>
                 
+                
               </BarChart>
+              
             </Center>
           </Box>
           <Button onClick={()=>{setDate("29/04/2021")}}>29/04/2021</Button>
           <Button onClick={()=>{setDate("02/05/2021")}}>02/05/2021</Button>
-          <b>test test</b>
-          {data.map((entry, index) => {
-            var entryLevelGauge = <div>
-              <b>{entry["errors-entry-level"]} {(entry["entry-level-score"]+5)/10}</b>
-              <GaugeChart 
-              id="gauge-chart-entry" 
-              arcsLength={getArcsLength(entry["errors-entry-level"], (entry["entry-level-score"]+5)/10)}
-              colors={["#FF5FFF", "#FFC3FF", "#FF5FFF"]} 
-              arcWidth={0.3} 
-              textColor="464A4F"
-              hideText={true}
-              percent={(entry["entry-level-score"]+5)/10} 
-            /></div>
-            if (entry["errors-follow-up"]) {
-              entryLevelGauge = <div>
-              {entryLevelGauge}
-              <GaugeChart 
-                  id="gauge-chart-follow-up" 
-                  arcsLength={getArcsLength(entry["errors-follow-up"], (entry["scaled-follow-up-score"]+5)/10)}
+          
+          <br/><br/>
+          <SimpleGrid columns={2} spacing={10}>
+            {data.map((entry, index) => {
+              var result = 
+              <Box>
+                <b>{entry["domain"]}</b>
+                <Box>Entry-level</Box>
+                {/* <b>{entry["errors-entry-level"][0]} {entry["errors-entry-level"][1]} {(entry["entry-level-score"]+5)/10}</b> */}
+                <GaugeChart 
+                  id="gauge-chart-entry" 
+                  arcsLength={getArcsLength(entry["errors-entry-level"], (entry["entry-level-score"]+5)/10)}
                   colors={["#FFC3FF", "#FF5FFF", "#FFC3FF"]} 
                   arcWidth={0.3} 
                   textColor="464A4F"
                   hideText={true}
-                  percent={(entry["scaled-follow-up-score"]+5)/10} 
+                  percent={(entry["entry-level-score"]+5)/10+0.01} 
+                  arcPadding={0}
+                />
+              </Box>
+              if (entry["errors-follow-up"]) {
+                result = <Box >
+                  {result}
+                  <Box>
+                    <Box>Follow-up</Box>
+                    {/* <b>{entry["errors-follow-up"][0]} {entry["errors-follow-up"][1]} {(entry["scaled-follow-up-score"]+5)/10}</b> */}
+                    <GaugeChart 
+                      id="gauge-chart-follow-up" 
+                      arcsLength={getArcsLength(entry["errors-follow-up"], (entry["scaled-follow-up-score"]+5)/10)}
+                      colors={["#FFC3FF", "#FF5FFF", "#FFC3FF"]} 
+                      arcWidth={0.3} 
+                      textColor="464A4F"
+                      hideText={true}
+                      percent={(entry["scaled-follow-up-score"]+5)/10+0.01} 
+                      arcPadding={0}
+                    /> 
+                  </Box>
+                </Box>
+              }
+              return (result)
+            })}
+          </SimpleGrid>
+          {/* <b>test test1 {data.length}</b> */}
+          {/* <ReactSpeedometer/> */}
+          {/* {data.map((entry, index) => {
+            //return <ReactSpeedometer/>
+            <b>test map</b>
+            })
+          } */}
+          {/* {data.map((entry, index) => {
+            console.log(entry)
+            return <div>
+              <b>{entry["errors-entry-level"][0]} {entry["errors-entry-level"][1]} {(entry["entry-level-score"]+5)/10}</b>
+              <ReactSpeedometer/>
+            </div>
+            })
+          } */}
+          {/* <b>test test1</b>
+          {data.map((entry, index) => {
+            var entryLevelGauge = <div>
+              <b>{entry["errors-entry-level"][0]} {entry["errors-entry-level"][1]} {(entry["entry-level-score"]+5)/10}</b>
+              <ReactSpeedometer  
+              segmentColors={["#FFC3FF", "#FF5FFF", "#FFC3FF"]} 
+              customSegmentStops={[0, (entry["entry-level-score"]+5)/10 - entry["errors-entry-level"][0],(entry["entry-level-score"]+5)/10 + entry["errors-entry-level"][1], 1]}
+              textColor="464A4F"
+              value={(entry["entry-level-score"]+5)/10} 
+              maxValue={1}
+            /></div>
+            if (entry["errors-follow-up"]) {
+              entryLevelGauge = <div>
+              {entryLevelGauge}
+              <b>{entry["errors-follow-up"][0]} {entry["errors-follow-up"][1]} {(entry["scaled-follow-up-score"]+5)/10}</b>
+              <ReactSpeedometer   
+                  customSegmentStops={[0, (entry["scaled-follow-up-score"]+5)/10 - entry["errors-follow-up"][0],(entry["scaled-follow-up-score"]+5)/10 + entry["errors-follow-up"][1], 1]}
+                  segmentColors={["#FFC3FF", "#FF5FFF", "#FFC3FF"]} 
+                  textColor="464A4F"
+                  value={(entry["scaled-follow-up-score"]+5)/10} 
+                  maxValue={1}
                 /> </div>
             }
             return (entryLevelGauge)
-          })}
+          })} */}
           
         </VStack>
       </MainContainer>
