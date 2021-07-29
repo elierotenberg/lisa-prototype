a2c240cedba5d4a2c4e17c902b6fcc2903d66a4e
charlie42 <oninfrared@gmail.com>
2021-07-29 14:12:43 +0200

    transform json test

--- src/pages/csv-demo.tsx
+++ src/pages/csv-demo.tsx
@@ -13,6 +13,13 @@ import { MainContainer } from "../components/MainContainer/MainContainer";
 
 import Papa from "papaparse";
 
+var student = ""
+
+const domainsByCategory = {
+  "Behavior": ["habits and repetitive behaviours", "risk taking"],
+  "Cognition": ["attention", "planning and organization"],
+}
+
 const CsvDemoVisualization: FunctionComponent<{ data: unknown[] }> = ({
   data,
 }) => {
@@ -42,6 +49,7 @@ const CsvDemo: FunctionComponent = () => {
       </Head>
       <MainContainer pt={4}>
         <VStack spacing={4} alignItems="stretch">
+          <h>{student}</h>
           <Flex justifyContent="center">
             <input type="file" onChange={onChangeFile} />
           </Flex>
@@ -58,11 +66,52 @@ const CsvDemo: FunctionComponent = () => {
               ))}
             </Flex>
           )}
-          {result?.data && <CsvDemoVisualization data={result.data} />}
+          {result?.data && <CsvDemoVisualization data={transformData(result.data)} />}
         </VStack>
       </MainContainer>
     </Fragment>
   );
 };
+
+const transformData = (data: unknown[]) => {
+  var cleanedData = data
+  
+  // remove headers
+  cleanedData.shift()
+
+  // get student ID
+  console.log(cleanedData[0])
+  student = cleanedData[0][10]
+  
+  // only keep last assessment
+  const latestEndTime = cleanedData[0][3]
+  cleanedData = cleanedData.filter(item => item[3] == latestEndTime);
+  
+  // parse value
+  cleanedData.map(item => item.push(item[10].split(": ")[1]))
+
+  var result = []
+  Object.keys(domainsByCategory).forEach(cateogryName => {
+    var categoryObject = {}
+    categoryObject[cateogryName] = []
+    domainsByCategory[cateogryName].forEach(domainName => {
+      var score = getScoreByDomain(cleanedData, domainName)
+      var domainObject = {
+        "domain": domainName,
+        "score": score
+      }
+      categoryObject[cateogryName].push(domainObject)
+    })
+    result.push(categoryObject)
+  })
+  
+  // don't forget bullying
+  return result
+}
+
+const getScoreByDomain = (data: unknown[], domainName) => {
+  var score = data.filter(item => item[11].toLowerCase() == domainName)[0][15]
+  return score
+}
 
 export default CsvDemo;
